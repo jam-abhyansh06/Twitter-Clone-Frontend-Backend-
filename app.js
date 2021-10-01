5afa2c9dd2208ffbba2164417e09d719a5bd74ff
jam-abhyansh06 <abhyansh.5959@gmail.com>
2021-10-02 00:57:23 +0530

    Registration page added & styled

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const loginRoute = require("./routes/loginRoutes")
+const registerRoute = require("./routes/registerRoutes")
 
-app.use(loginRoute)
+app.use("/login",loginRoute)
+app.use("/register",registerRoute)
+
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
