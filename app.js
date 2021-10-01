367c5ae9bdb064798542c1002d8caad303fccac8
jam-abhyansh06 <abhyansh.5959@gmail.com>
2021-10-02 01:40:50 +0530

    Added validation for empty fields and for password confirm

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express")
-const app = express()
-
 const middleware = require("./middleware")
-
 const path = require("path")
+const bodyParser = require("body-parser")
+
+const app = express()
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 // Routes
 const loginRoute = require("./routes/loginRoutes")
