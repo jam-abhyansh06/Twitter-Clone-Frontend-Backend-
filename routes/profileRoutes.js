023baf6cc9f2d45998af6c64df94c787b2604210
jam-abhyansh06 <abhyansh.5959@gmail.com>
2021-10-06 21:57:17 +0530

    setup /profile/:username to access profile using username

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -9,7 +9,6 @@ const router = express.Router()
 app.use(bodyParser.urlencoded({extended: true}))
 
 
-
 router.get("/", (req, res, next) => {
 
     let payload ={
@@ -22,6 +21,36 @@ router.get("/", (req, res, next) => {
     res.status(200).render("profilePage", payload);
   
 })
+
+
+router.get("/:username", async (req, res, next) => {
+
+    let payload = await getPayload(req.params.username, req.session.user);
+
+    res.status(200).render("profilePage", payload);
+  
+})
+
+
+async function getPayload(username, userLoggedIn) {
+    let user = await User.findOne({username : username});   // find user in DB
+
+    // if username param in url is not in DB
+    if(user === null) {
+        return {
+            pageTitle: "User not found",
+            userLoggedIn: userLoggedIn,
+            userLoggedInJs: JSON.stringify(userLoggedIn),
+        }
+    }
+
+    return {
+        pageTitle: user.username,
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+        profileUser: user
+    }
+}
 
 
 module.exports = router;
