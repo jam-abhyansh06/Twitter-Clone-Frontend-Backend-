b31ca5ef97734c23c21480870d9d476f949db520
jam-abhyansh06 <abhyansh.5959@gmail.com>
2021-10-08 09:24:16 +0530

    checked iof already following & then updating accordingly

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,7 +18,35 @@ router.put("/:userId/follow", async (req, res, next) => {
     
     let isFollowing = user.followers && user.followers.includes(req.session.user._id);
 
-    res.status(200).send(isFollowing)
+    let option = isFollowing ? "$pull" : "$addToSet"
+ 
+
+    // Insert or Delete user followers
+    // {new: true} gives new updated user object and assigning it to req.session.user caches it so that unliking features work properly
+    
+    // update following of the user following
+    await User.findByIdAndUpdate(req.session.user._id, { [option] : {following: userId} }, {new: true})
+    .then(result => req.session.user=result)
+    .catch(error=> {
+        console.log(error);
+        res.sendStatus(400);
+    })
+    // update follower of the user being followed
+    User.findByIdAndUpdate(userId, { [option] : {followers: req.session.user._id} })
+    .then()
+    .catch(error=> {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    // Insert or Delete post likes
+
+    // let post = await Post.findByIdAndUpdate(postId, { [option] : {likes: userId} }, {new: true})
+    // .catch(error=> {
+    //     console.log(error);
+    //     res.sendStatus(400);
+    // })
+    res.status(200).send(req.session.user)
 })
 
 
